d6efa485e3a17d6006d469b0a98d99f6dcacc1a2
jing.dong <dj3386083@gmail.com>
2018-12-23 22:45:18 +1100

    update

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const insert = function (num) {
+  document.form.textview.value = document.form.textview.value + num;
+};
+
+const equal = function () {
+  let exp = document.form.textview.value
+  if(exp) {
+    document.form.textview.value = eval(exp);
+  }
+};
+
+const clean = function () {
+  document.form.textview.value="";
+};
+
+const back = function () {
+  let exp = document.form.textview.value;
+  document.form.textview.value = exp.substring(0,exp.length-1);
+};
